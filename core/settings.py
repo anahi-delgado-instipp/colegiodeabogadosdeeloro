896863cd19803aea9de2e54c0becb7dcb47a6167
AnahiDelgado <adelgado316_est@instipp.edu.ec>
2026-01-08 11:32:15 -0500

    act codigo

--- core/settings.py
+++ core/settings.py
@@ -13,10 +13,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config(
-    'SECRET_KEY',
-    default='django-insecure-temporal-para-build'
-)
+SECRET_KEY = config('SECRET_KEY')
+
 
 
 
